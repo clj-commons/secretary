acf832f759ae3db971bdfd86bfe155b8675d3405
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-10-01 11:12:49 -0700

    Minor refactoring

--- src/secretary/core.cljs
+++ src/secretary/core.cljs
@@ -3,22 +3,23 @@
 
 (def ^:dynamic *routes* (atom {}))
 
-(def ^:private slash (new js/RegExp "/"))
+(def ^:private slash #"/")
+
+(defn- param? [r]
+  (= (first r) \:))
 
 (defn- component-matches? [r u]
-  (if (= (first r) \:)
-    true
-    (= r u)))
+  (or (param? r) (= r u)))
 
 (defn- extract-component [r u]
-  (when (= (first r) \:)
-    {(keyword (apply str (rest r))) u}))
+  (when (param? r) 
+    {(keyword (subs r 1)) u}))
 
 (defn- exact-match? [r u]
   (= r u))
 
 (defn route-matches?
-  "A predicate to determine if a route matches a URI path"
+  "A predicate to determine if a route matches a URI path."
   [route uri-path]
   (let [r (string/split route slash)
         u (string/split uri-path slash)]
@@ -26,7 +27,7 @@
       (every? true? (map #(component-matches? %1 %2) r u)))))
 
 (defn any-matches?
-  "Determines if there are any routes that match a given URI path"
+  "Determines if there are any routes that match a given URI path."
   [uri-path]
   (some #(route-matches? (first %) uri-path) @*routes*))
 
@@ -40,11 +41,15 @@
                  :when (not (nil? c))]
              c))))
 
+(defn find-route [uri-path]
+  (first (filter #(exact-match? (first %) uri-path) @*routes*)))
+
 (defn dispatch!
   "Dispatch an action for a given route if it matches the URI path"
   [uri-path]
-  (if-let [[route action] (first (filter #(exact-match? (first %) uri-path) @*routes*))]
+  (if-let [[route action] (find-route uri-path)]
     (action {})
-    (when-first [[route action] (filter #(route-matches? (first %) uri-path) @*routes*)]
+    (when-first [[route action] (find-route uri-path)]
       (action (extract-components route uri-path)))))
+
 
