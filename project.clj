fde1112c6fc39e36c2bfc75be897efb1157dc4db
Gianni Chiappetta <gianni@runlevel6.org>
2013-10-01 12:22:39 -0700

    Bump version to 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject secretary "0.2.0"
+(defproject secretary "0.3.0"
   :description "A client-side router for ClojureScript."
   :url "https://github.com/gf3/secretary"
   :license {:name "Eclipse Public License - v 1.0"
