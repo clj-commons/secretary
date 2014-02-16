03aaa39120eab4f1ad7b07e551948c8666b95bfe
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-02-15 17:27:48 -0800

    Reorganize tests, add tests for regex routes

--- test/secretary/test/core.cljs
+++ test/secretary/test/core.cljs
@@ -1,94 +1,58 @@
 (ns secretary.test.core
-  (:require [cemerick.cljs.test :as t]
+  (:require [cemerick.cljs.test]
             [secretary.core :as secretary :include-macros true :refer [defroute]])
   (:require-macros [cemerick.cljs.test :refer [deftest is are testing]]))
 
-;; Test helpers
 
-(defn compile-and-match [r t]
-  (-> (secretary/compile-route r)
-      (secretary/route-matches t)))
+(deftest query-params-test
+  (testing "encodes query params"
+    (let [query-params {"id" "kevin" "food" "bacon"}
+          encoded (secretary/encode-query-params query-params)]
+      (is (= (secretary/decode-query-params encoded)
+             query-params))))
 
-;; Tests
+  (testing "decodes query params"
+    (let [query-string "id=kevin&food=bacong"
+          decoded (secretary/decode-query-params query-string)
+          encoded (secretary/encode-query-params decoded)]
+      (is (re-find #"id=kevin" query-string))
+      (is (re-find #"food=bacon" query-string)))))
 
-(deftest route-test 
-  (is (= 1 1))
-  (testing "dispatch!"
-    (secretary/reset-routes!)
-    (defroute "/" [] "BAM!")
-    (defroute "/users" [] "ZAP!")
-    (defroute "/users/:id" {:as params} params)
-    (defroute "/users/:id/food/:food" {:as params} params)
 
-    (is (= (secretary/dispatch! "/")
-           "BAM!"))
-    (is (= (secretary/dispatch! "/users")
-           "ZAP!"))
-    (is (= (secretary/dispatch! "/users/1")
-           {:id "1"}))
-    (is (= (secretary/dispatch! "/users/kevin/food/bacon")
-           {:id "kevin", :food "bacon"})))
-
-  (testing "named routes"
-    (secretary/reset-routes!)
-
-    (defroute food-route "/food/:food" {:keys [food]})
-
-    (is (fn? food-route))
-    (is (fn? (defroute "/pickles" {})))
-    (is (= (food-route {:food "biscuits"})
-           "/food/biscuits")))
-
-  (testing "query-params"
-    (secretary/reset-routes!)
-    (defroute "/search-1" {:as params} params)
-    (defroute "/search-2" [query-params] query-params)
-
-    (is (not (contains? (secretary/dispatch! "/search-1")
-                        :query-params)))
-    (is (contains? (secretary/dispatch! "/search-1?foo=bar")
-                   :query-params))
-
-    (let [s "abc123 !@#$%^&*"
-          [p1 p2] (take 2 (iterate #(apply str (shuffle %)) s))
-          r (str "/search-2?"
-                 "foo=" (js/encodeURIComponent p1)
-                 "&bar=" (js/encodeURIComponent p2))]
-      (is (= (secretary/dispatch! r)
-             {"foo" p1 "bar" p2}))))
-
+(deftest route-matches-test 
   (testing "non-encoded-routes"
-    (is (compile-and-match "/foo bar baz" "/foo%20bar%20baz")))
+    (is (secretary/route-matches "/foo bar baz" "/foo%20bar%20baz")))
 
   (testing "utf-8 routes"
-    (is (= (compile-and-match "/:x" "/%E3%81%8A%E3%81%AF%E3%82%88%E3%81%86")
+    (is (= (secretary/route-matches "/:x" "/%E3%81%8A%E3%81%AF%E3%82%88%E3%81%86")
            {:x "おはよう"})))
 
+  (testing "regex-routes"
+    (secretary/reset-routes!)
+
+    (is (= (secretary/route-matches #"/([a-z]+)/(\d+)" "/lol/420")
+           ["lol" "420"]))
+    (is (not (secretary/route-matches #"/([a-z]+)/(\d+)" "/0x0A/0x0B"))))
+
   (testing "splats"
-    (is (= (compile-and-match "*" "")
+    (is (= (secretary/route-matches "*" "")
            {:* ""}))
-    (is (= (compile-and-match "*" "/foo/bar")
+    (is (= (secretary/route-matches "*" "/foo/bar")
            {:* "/foo/bar"}))
-    (is (= (compile-and-match "*.*" "cat.bat")
+    (is (= (secretary/route-matches "*.*" "cat.bat")
            {:* ["cat" "bat"]}))
-    (is (= (compile-and-match "*path/:file.:ext" "/loller/skates/xxx.zip")
+    (is (= (secretary/route-matches "*path/:file.:ext" "/loller/skates/xxx.zip")
            {:path "/loller/skates"
             :file "xxx"
             :ext "zip"}))
-    (is (= (compile-and-match "/*a/*b/*c" "/lol/123/abc/look/at/me")
+    (is (= (secretary/route-matches "/*a/*b/*c" "/lol/123/abc/look/at/me")
            {:a "lol"
             :b "123"
             :c "abc/look/at/me"}))))
 
-(deftest encode-query-params-test
-  (testing "handles query params"
-    (let [query-params {"id" "kevin" "food" "bacon"}
-          encoded (secretary/encode-query-params query-params)]
-      (is (= (secretary/decode-query-params encoded)
-             query-params)))))
 
 (deftest render-route-test
-  (testing "interpolates correctly"
+  (testing "it interpolates correctly"
     (is (= (secretary/render-route "/")
            "/"))
     (is (= (secretary/render-route "/users/:id" {:id 1})
@@ -99,6 +63,8 @@
            "/users/123"))
     (is (= (secretary/render-route "/users/:id" {:id 123 :query-params {:page 2 :per-page 10}})
            "/users/123?page=2&per-page=10"))
+    (is (= (secretary/render-route "/:id/:id" {:id 1})
+           "/1/1"))
     (is (= (secretary/render-route "/:id/:id" {:id [1 2]})
            "/1/2"))
     (is (= (secretary/render-route "/*id/:id" {:id [1 2]})
@@ -115,6 +81,63 @@
       (is (= (secretary/render-route "/users/:id" {:id 1})
              "#/users/1"))))
   
-  (testing "leaves param in string if not in map"
+  (testing "it leaves param in string if not in map"
     (is (= (secretary/render-route "/users/:id" {})
            "/users/:id"))))
+
+
+(deftest defroute-test
+  (testing "dispatch! with basic routes"
+    (secretary/reset-routes!)
+
+    (defroute "/" [] "BAM!")
+    (defroute "/users" [] "ZAP!")
+    (defroute "/users/:id" {:as params} params)
+    (defroute "/users/:id/food/:food" {:as params} params)
+
+    (is (= (secretary/dispatch! "/")
+           "BAM!"))
+    (is (= (secretary/dispatch! "/users")
+           "ZAP!"))
+    (is (= (secretary/dispatch! "/users/1")
+           {:id "1"}))
+    (is (= (secretary/dispatch! "/users/kevin/food/bacon")
+           {:id "kevin", :food "bacon"})))
+
+  (testing "dispatch! with query-params"
+    (defroute "/search-1" {:as params} params)
+    (defroute "/search-2" [query-params] query-params)
+
+    (is (not (contains? (secretary/dispatch! "/search-1")
+                        :query-params)))
+    (is (contains? (secretary/dispatch! "/search-1?foo=bar")
+                   :query-params))
+    (let [s "abc123 !@#$%^&*"
+          [p1 p2] (take 2 (iterate #(apply str (shuffle %)) s))
+          r (str "/search-2?"
+                 "foo=" (js/encodeURIComponent p1)
+                 "&bar=" (js/encodeURIComponent p2))]
+      (is (= (secretary/dispatch! r)
+             {"foo" p1 "bar" p2}))))
+
+  (testing "dispatch! with regex routes"
+    (secretary/reset-routes!)
+    (defroute #"/([a-z]+)/(\d+)" [letters digits] [letters digits])
+
+    (is (= (secretary/dispatch! "/xyz/123")
+           ["xyz" "123"])))
+
+  (testing "named routes"
+    (secretary/reset-routes!)
+
+    (defroute food-path "/food/:food" [food])
+    (defroute search-path "/search" [query-params])
+
+    (is (fn? food-path))
+    (is (fn? (defroute "/pickles" {})))
+    (is (= (food-path {:food "biscuits"})
+           "/food/biscuits"))
+
+    (let [url (search-path {:query-params {:burritos 10, :tacos 200}})]
+      (is (re-find #"burritos=10" url))
+      (is (re-find #"tacos=200" url)))))
