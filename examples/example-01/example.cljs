81bbb05fbf2af710d258b3ff465e557ad6284f3c
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-02-16 00:33:44 -0800

    Create a basic example

---
+++ examples/example-01/example.cljs
@@ -0,0 +1,41 @@
+(ns example
+  (:require [secretary.core :as secretary :include-macros true :refer [defroute]]
+            [goog.events :as events])
+  (:import goog.History
+           goog.history.EventType))
+
+(def application
+  (js/document.getElementById "application"))
+
+(defn set-html! [el content]
+  (aset el "innerHTML" content))
+
+(secretary/set-config! :prefix "#")
+
+;; /#/
+(defroute home-path "/" []
+  (set-html! application "<h1>OMG! YOU'RE HOME!</h1>"))
+
+;; /#/users
+(defroute user-path "/users" []
+  (set-html! application "<h1>USERS!</h1>"))
+
+;; /#/users/:id
+(defroute user-path "/users/:id" [id]
+  (let [message (str "<h1>HELLO USER <small>" id "</small>!</h1>")]
+    (set-html! application message)))
+
+;; /#/777
+(defroute jackpot-path "/777" []
+  (set-html! application "<h1>YOU HIT THE JACKPOT!</h1>"))
+
+;; Catch all
+(defroute "*" []
+  (set-html! application "<h1>LOL! YOU LOST!</h1>"))
+
+;; Quick and dirty history configuration.
+(let [h (History.)]
+  (goog.events/listen h EventType/NAVIGATE #(secretary/dispatch! (.-token %)))
+  (doto h (.setEnabled h true)))
+
+(secretary/dispatch! "/")
