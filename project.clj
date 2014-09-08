149e59bd74473ba0e6747afe806e54c3bc41251d
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-09-07 17:05:22 -0700

    Don't depend on a specific version of ClojureScript

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
 
   :dependencies
   [[org.clojure/clojure "1.5.1"]
-   [org.clojure/clojurescript "0.0-2156"]]
+   [org.clojure/clojurescript "0.0-2156" :scop "provided"]]
 
   :plugins
   [[lein-cljsbuild "1.0.2"]]
