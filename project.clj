ac89b1a2f42b7fb166f6a1e45245a7cc445fb7e9
Jozef Wagner <jozef.wagner@gmail.com>
2018-10-26 10:59:14 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject secretary "1.2.4-SNAPSHOT"
+(defproject wagjo/secretary "1.2.5"
   :description "A client-side router for ClojureScript."
   :url "https://github.com/gf3/secretary"
   :license {:name "Eclipse Public License - v 1.0"
