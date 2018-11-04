f6c1e9a95021e3ca3690fb5da2d8ae48e4df996e
Jozef Wagner <jozef.wagner@gmail.com>
2018-11-04 14:15:10 +0100

    Revert "Bump version"
    This reverts commit 3064e81097236200ded3a63aa088a2847074a92c.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wagjo/secretary "1.2.5"
+(defproject secretary "1.2.4-SNAPSHOT"
   :description "A client-side router for ClojureScript."
   :url "https://github.com/gf3/secretary"
   :license {:name "Eclipse Public License - v 1.0"
